a14a27ba28d605ea30ddf8e24ef85db905336ef8
daniel5u <98783818+nierurein@users.noreply.github.com>
2022-02-15 12:37:20 +0700

    feat: Login, register, and logout

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const methodOverride = require('method-override');
 const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
-const ExpressError = require('./utils/ExpressError');
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
 const session = require('express-session');
 const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -43,16 +47,24 @@ const sesssionConfig = {
 }
 app.use(session(sesssionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // flash middleware
 app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.errors = req.flash('error');
     next();
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
---
+++ routes/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync')
+const passport = require('passport');
+
+router.get('/register', (req, res) => {
+    res.render('users/register');
+});
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+    } catch (error) {
+        req.flash('error', error.message);
+        res.redirect('/register');
+    }
+}));
+
+router.get('/login', async (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+});
+
+router.get('/logout', async (req, res) => {
+    req.logout();
+    req.flash('success', 'Goodbye');
+    res.redirect('/campgrounds');
+});
+
+module.exports = router;
