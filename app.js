0edc4e26617ae5ee3a70e265be8cfa230da3ea3c
daniel5u <98783818+nierurein@users.noreply.github.com>
2022-02-18 20:50:20 +0700

    fix: Modify package.json and app port for deployment to Heroku

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ db.once('open', () => {
 });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
